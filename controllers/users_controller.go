452b970dbf5dfc15886ec57aea0acb58208dcdbe
Dmitrii <dimau777@gmail.com>
2021-12-19 18:41:23 +0300

    Feature/adv testing (#3)
    * play with sorting and behchmarking
    
    * adding new unit tests and modified codebase for this purpose for user

--- controllers/users_controller.go
+++ controllers/users_controller.go
@@ -23,7 +23,7 @@ func GetUser(resp http.ResponseWriter, req *http.Request) {
 		return
 	}
 
-	user, apiErr := services.GetUser(userId)
+	user, apiErr := services.UsersService.GetUser(userId)
 	if apiErr != nil {
 		jsonValue, _ := json.Marshal(apiErr)
 		resp.WriteHeader(apiErr.StatusCode)
--- domain/user_dao.go
+++ domain/user_dao.go
@@ -11,9 +11,25 @@ var (
 	users = map[int64]*User{
 		123: {Id: 123, FirstName: "Dmitrii", LastName: "Ushakov", Email: "dimau@dfdf.ru"},
 	}
+
+	// Объявляем публичную переменную UserDAO для работы с пользователями в БД
+	// И при объявлении говорим, что эта переменная может содержать значения любых типов, поддерживающих интерфейс usersDAOInterface
+	UserDAO usersDAOInterface
 )
 
-func GetUser(userId int64) (*User, *utils.ApplicationError) {
+func init() {
+	// При инициализации записываем в нашу публичную переменную значение с типом userDAO
+	// Это позволит сервисам при обращении к этой переменной вызывать метод GetUser объявленный для данного типа
+	UserDAO = &userDAO{}
+}
+
+type usersDAOInterface interface {
+	GetUser(int64) (*User, *utils.ApplicationError)
+}
+
+type userDAO struct{}
+
+func (u *userDAO) GetUser(userId int64) (*User, *utils.ApplicationError) {
 	user := users[userId]
 	if user == nil {
 		return nil, &utils.ApplicationError{
